f17383eed4a037434dd1a9cbd64b5b7959ac98e5
Gustavo <gustavobretas@gmail.com>
2020-04-04 14:48:08 -0400

    Curso JavaScript - Introdução ao Javascript

---
+++ 1-introducao/exercicio01.js
@@ -0,0 +1,10 @@
+var endereco = {
+    rua: "Rua dos pinheiros",
+    numero: 1293,
+    bairro: "Centro",
+    cidade: "São Paulo",
+    uf: "SP"
+   };
+
+   console.log('O usuário mora em %s / %s, no bairro %s, na rua "%s" com nº %s.',
+    endereco.cidade, endereco.uf, endereco.bairro, endereco.rua, endereco.numero);

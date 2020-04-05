f13c5303ba4c7a7dc4a382096db75d45f7e3284e
Gustavo <gustavobretas@gmail.com>
2020-04-05 11:39:29 -0400

    Iniciando Aplicação

--- 3-ToDo/todo.js
+++ 3-ToDo/todo.js
@@ -1,1 +1,9 @@
-alert('Funcionou!');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var btnElement = document.querySelector('#app button');
+ 
+var todos = [
+    'Fazer Café', 
+    'Estudar Javascript', 
+    'Entrar na Comunidade da Rocketseat'
+];

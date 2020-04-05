8ab4d625fb94818154f14a7c5850bf97352d8c89
Gustavo <gustavobretas@gmail.com>
2020-04-05 16:03:05 -0400

    Exercício 2

--- 4-jsAssincrono/main.js
+++ 4-jsAssincrono/main.js
@@ -9,7 +9,7 @@ function execAjax(){
         });
 }
 
-async function checaIdade(idade) {
+function checaIdade(idade) {
     return new Promise(function(resolve, reject){
         setTimeout(() => {  
             if (idade >= 18) {
@@ -31,3 +31,33 @@ document.getElementById('verificarIdade').onclick = function () {
         console.log(error);
         });    
 };
+
+function addUsuario(username) {
+    function addRepos(repos){
+        var h2Element = document.createElement('h2');
+        h2Element.innerHTML = username;
+        document.getElementById('app').appendChild(h2Element);
+        var ulElement = document.createElement('ul');
+        for (repo of repos) {
+            var liElement = document.createElement('li');
+            var aElement = document.createElement('a');
+            aElement.setAttribute('href', repo.html_url)
+            aElement.innerHTML = repo.name;
+            liElement.appendChild(aElement);
+            ulElement.appendChild(liElement);
+            document.getElementById('app').appendChild(ulElement);
+        }
+    }
+    axios.get('https://api.github.com/users/'+username+'/repos')
+        .then(function(response) {
+            addRepos(response.data);
+        })
+        .catch(function(error) {
+            console.warn(error)
+        });
+};
+
+document.getElementById('btnAddUsuario').onclick = function () {
+    addUsuario(document.getElementById('inputUsuario').value);
+    document.getElementById('inputUsuario').value = '';
+}

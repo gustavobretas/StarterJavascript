57ac53c2e613f2fd5e81737c7e897d7f1f715038
Gustavo <gustavobretas@gmail.com>
2020-04-05 11:48:08 -0400

    Renderizando ToDos

--- 3-ToDo/todo.js
+++ 3-ToDo/todo.js
@@ -7,3 +7,15 @@ var todos = [
     'Estudar Javascript', 
     'Entrar na Comunidade da Rocketseat'
 ];
+
+function renderToDos() {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+        
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderToDos()

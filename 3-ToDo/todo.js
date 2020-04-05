15e459b815deb2ad00f0e7e55fbccd735badfa81
Gustavo <gustavobretas@gmail.com>
2020-04-05 12:07:34 -0400

    Excluindo ToDos

--- 3-ToDo/todo.js
+++ 3-ToDo/todo.js
@@ -10,12 +10,22 @@ var todos = [
 
 function renderToDos() {
     listElement.innerHTML = '';
-    
+
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#')
+        var linkText = document.createTextNode('Excluir');
+        
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo('+pos+')');
+
+        linkElement.appendChild(linkText);
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
     }
 }
@@ -33,3 +43,8 @@ function addTodo() {
 }
 
 btnElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderToDos();
+}

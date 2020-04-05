6a6d88ad96c28d6846606ddb6818b1511801515b
Gustavo <gustavobretas@gmail.com>
2020-04-05 15:28:40 -0400

    Ecercício 1

--- 4-jsAssincrono/main.js
+++ 4-jsAssincrono/main.js
@@ -1,8 +1,33 @@
-axios.get('https://api.github.com/users/gustavobretas')
-    .then(function(response) {
-        // console.log(response.data.avatar_url)
-        console.log(response)
+function execAjax(){
+    axios.get('https://api.github.com/users/gustavobretas')
+        .then(function(response) {
+            // console.log(response.data.avatar_url)
+            console.log(response)
+        })
+        .catch(function(error) {
+            console.warn(error)
+        });
+}
+
+async function checaIdade(idade) {
+    return new Promise(function(resolve, reject){
+        setTimeout(() => {  
+            if (idade >= 18) {
+                resolve("Maior que 18 anos!")
+            } else {
+                reject("Menor que 18 anos!")
+            };
+        }, 2000);
     })
-    .catch(function(error) {
-        console.warn(error)
-    });
+};
+    
+
+document.getElementById('verificarIdade').onclick = function () {
+    checaIdade(document.getElementById('inputIdade').value)
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+        console.log(error);
+        });    
+};

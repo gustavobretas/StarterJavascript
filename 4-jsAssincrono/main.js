9b479a4a7438c929f0a06e65bd12291752d25a69
Gustavo <gustavobretas@gmail.com>
2020-04-05 12:47:10 -0400

    Utilizando Axios

--- 4-jsAssincrono/main.js
+++ 4-jsAssincrono/main.js
@@ -1,25 +1,6 @@
-var minhaPromise = function () {
-    return new Promise(function(resolve, reject){
-
-        var xhr = new XMLHttpRequest();
-
-        xhr.open('GET', 'https://api.github.com/users/gustavobretas');
-        xhr.send(null);
-        
-        xhr.onreadystatechange = function(){
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else (
-                    reject('Erro na Rquisição')
-                )
-            }
-        }
-    })
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/gustavobretas')
     .then(function(response) {
+        // console.log(response.data.avatar_url)
         console.log(response)
     })
     .catch(function(error) {
